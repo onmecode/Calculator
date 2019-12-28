c00fc79a4f70e89d9c42f8193a105f10946e7be1
Ireaurora <ireaurora@gmail.com>
2019-12-28 00:32:21 +0000

    Only added a small response but missing the values

--- calculator.js
+++ calculator.js
@@ -1,45 +1,71 @@
+//actual working calculator
+
+
+
+
 
 //using keys on the keyboard instead of the display keys
 
 //1
 document.addEventListener("keydown", () =>{
-               if(event.keyCode === 49)
+               if(event.keyCode === 49){
+                 document.getElementById("").style.borderColor = "blue";
+               }
            });
 // 2
 document.addEventListener("keydown", () =>{
-               if(event.keyCode === 50)
+               if(event.keyCode === 50){
+                 document.getElementById("").style.borderColor = "blue";
+               }
            });
 //3
 document.addEventListener("keydown", () =>{
-               if(event.keyCode === 51)
+               if(event.keyCode === 51){
+                 document.getElementById("").style.borderColor = "blue";
+               }
            });
 //4
 document.addEventListener("keydown", () =>{
-               if(event.keyCode === 52)
+               if(event.keyCode === 52){
+                 document.getElementById("").style.borderColor = "blue";
+               }
            });
 //5
 document.addEventListener("keydown", () =>{
-               if(event.keyCode === 53)
+               if(event.keyCode === 53){
+                 document.getElementById("").style.borderColor = "blue";
+               }
            });
 //6
 document.addEventListener("keydown", () =>{
-               if(event.keyCode === 54)
+               if(event.keyCode === 54){
+                 document.getElementById("").style.borderColor = "blue";
+               }
            });
 //7
 document.addEventListener("keydown", () =>{
-               if(event.keyCode === 55)
+               if(event.keyCode === 55){
+                 document.getElementById("").style.borderColor = "blue";
+               }
            });
 //8
 document.addEventListener("keydown", () =>{
-               if(event.keyCode === 56)
+               if(event.keyCode === 56){
+                 document.getElementById("").style.borderColor = "blue";
+               }
            });
 //9
 document.addEventListener("keydown", () =>{
-               if(event.keyCode === 57)
+               if(event.keyCode === 57){
+                 document.getElementById("").style.borderColor = "blue";
+               }
+
            });
 //0
 document.addEventListener("keydown", () =>{
-               if(event.keyCode === 48)
+               if(event.keyCode === 48){
+                 document.getElementById("").style.borderColor = "blue";
+               }
            });
 
 //- might  not work
@@ -47,20 +73,26 @@ document.addEventListener("keydown", () =>{
                if(event.keyCode === 16){
                // +
                  if(event.keyCode == 187){
+                   document.getElementById("").style.borderColor = "blue";
 
                  }
                  // -
                  if(event.keyCode === 189){
+                   document.getElementById("").style.borderColor = "blue";
 
                  }
                }
            });
 // /
 document.addEventListener("keydown", () =>{
-               if(event.keyCode === 191)
+               if(event.keyCode === 191){
+                 document.getElementById("").style.borderColor = "blue";
+               }
            });
 
 // =
 document.addEventListener("keydown", () =>{
-               if(event.keyCode === 187)
+               if(event.keyCode === 187){
+                 document.getElementById("").style.borderColor = "blue";
+               }
            });
