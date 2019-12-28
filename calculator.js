c521318b4893affa7fc0694de6c1f14481aa36ef
Ireaurora <ireaurora@gmail.com>
2019-12-28 00:19:48 +0000

    Added some event listeners

---
+++ calculator.js
@@ -0,0 +1,66 @@
+
+//using keys on the keyboard instead of the display keys
+
+//1
+document.addEventListener("keydown", () =>{
+               if(event.keyCode === 49)
+           });
+// 2
+document.addEventListener("keydown", () =>{
+               if(event.keyCode === 50)
+           });
+//3
+document.addEventListener("keydown", () =>{
+               if(event.keyCode === 51)
+           });
+//4
+document.addEventListener("keydown", () =>{
+               if(event.keyCode === 52)
+           });
+//5
+document.addEventListener("keydown", () =>{
+               if(event.keyCode === 53)
+           });
+//6
+document.addEventListener("keydown", () =>{
+               if(event.keyCode === 54)
+           });
+//7
+document.addEventListener("keydown", () =>{
+               if(event.keyCode === 55)
+           });
+//8
+document.addEventListener("keydown", () =>{
+               if(event.keyCode === 56)
+           });
+//9
+document.addEventListener("keydown", () =>{
+               if(event.keyCode === 57)
+           });
+//0
+document.addEventListener("keydown", () =>{
+               if(event.keyCode === 48)
+           });
+
+//- might  not work
+document.addEventListener("keydown", () =>{
+               if(event.keyCode === 16){
+               // +
+                 if(event.keyCode == 187){
+
+                 }
+                 // -
+                 if(event.keyCode === 189){
+
+                 }
+               }
+           });
+// /
+document.addEventListener("keydown", () =>{
+               if(event.keyCode === 191)
+           });
+
+// =
+document.addEventListener("keydown", () =>{
+               if(event.keyCode === 187)
+           });
