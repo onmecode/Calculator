df6e29cbea9c113f1b660e564184335694d906eb
Sky Get in Tech <onmecode@gmail.com>
2019-12-28 01:28:23 +0000

    adding dig

--- calculator.js
+++ calculator.js
@@ -1,98 +1,43 @@
-//actual working calculator
+const calculator = {
+  displayValue: '0',
+  firstOperand: "",
+  waitingForSecondOperand: false,
+  operator: "",
+};
 
+function updateDisplay() {
+  const display = document.querySelector('.calculator-screen');
+  display.value = calculator.displayValue;
+}
 
+updateDisplay();
 
+const keys = document.querySelector('.calculator-keys');
 
+keys.addEventListener('click', (event) => {
+  const { target } = event;
 
-//using keys on the keyboard instead of the display keys
+  if (!target.matches('button')) {
+    return;
+  }
 
-//1
-document.addEventListener("keydown", () =>{
-               if(event.keyCode === 49){
-                 document.getElementById("").style.borderColor = "blue";
-               }
-           });
-// 2
-document.addEventListener("keydown", () =>{
-               if(event.keyCode === 50){
-                 document.getElementById("").style.borderColor = "blue";
-               }
-           });
-//3
-document.addEventListener("keydown", () =>{
-               if(event.keyCode === 51){
-                 document.getElementById("").style.borderColor = "blue";
-               }
-           });
-//4
-document.addEventListener("keydown", () =>{
-               if(event.keyCode === 52){
-                 document.getElementById("").style.borderColor = "blue";
-               }
-           });
-//5
-document.addEventListener("keydown", () =>{
-               if(event.keyCode === 53){
-                 document.getElementById("").style.borderColor = "blue";
-               }
-           });
-//6
-document.addEventListener("keydown", () =>{
-               if(event.keyCode === 54){
-                 document.getElementById("").style.borderColor = "blue";
-               }
-           });
-//7
-document.addEventListener("keydown", () =>{
-               if(event.keyCode === 55){
-                 document.getElementById("").style.borderColor = "blue";
-               }
-           });
-//8
-document.addEventListener("keydown", () =>{
-               if(event.keyCode === 56){
-                 document.getElementById("").style.borderColor = "blue";
-               }
-           });
-//9
-document.addEventListener("keydown", () =>{
-               if(event.keyCode === 57){
-                 document.getElementById("").style.borderColor = "blue";
-               }
+  if (target.classList.contains('operator')) {
+    console.log('operator', target.value);
+    return;
+  }
 
-           });
-//0
-document.addEventListener("keydown", () =>{
-               if(event.keyCode === 48){
-                 document.getElementById("").style.borderColor = "blue";
-               }
-           });
+  if (target.classList.contains('all-clear')) {
+    console.log('clear', target.value)
+    return;
+  }
 
-//- might  not work
-document.addEventListener("keydown", () =>{
-               if(event.keyCode === 16){
-               // +
-                 if(event.keyCode == 187){
-                   document.getElementById("").style.borderColor = "blue";
+  inputDigit (target.value);
+  updateDisplay();
+});
 
-                 }
-                 // -
-                 if(event.keyCode === 189){
-                   document.getElementById("").style.borderColor = "blue";
+function inputDigit(digit) {
 
-                 }
-               }
-           });
-// /
-document.addEventListener("keydown", () =>{
-               if(event.keyCode === 191){
-                 document.getElementById("").style.borderColor = "blue";
-               }
-           });
+  const { displayValue } = calculator;
+  calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
 
-// =
-document.addEventListener("keydown", () =>{
-               if(event.keyCode === 187){
-                 document.getElementById("").style.borderColor = "blue";
-               }
-           });
+}
